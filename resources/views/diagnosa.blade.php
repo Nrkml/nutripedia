eee16271a7a48303c4627aaa475820b12929d82a
Nurakmal r <Nurakmal.ramadhan09@gmail.com>
2023-07-16 00:32:46 +0700

    nambah diagnosa

---
+++ resources/views/diagnosa.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main id="main">
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/diagnosa/head.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Selamat Datang</h2>
+                <p class="animate__animated animate__fadeInUp">Mulai diagnosa?</p>
+                <div>
+                <a href="#" class="btn-get-started animate__animated animate__fadeInUp scrollto">Mulai</a>
+                  <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+</main><!-- End #main -->
+
+@stop
