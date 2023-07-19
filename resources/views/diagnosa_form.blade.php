cc0fb49a8873ab771171a762cf74254f09bdfd5a
Nurakmal r <Nurakmal.ramadhan09@gmail.com>
2023-07-19 22:52:37 +0700

    form diagnosa

---
+++ resources/views/diagnosa_form.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main id="main">
+<br><br><br>
+  
+
+  
+
+  <!-- ======= Why Us Section ======= -->
+  <section id="why-us" class="why-us">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Diagonosa masalah pada bayi</h2>
+        <h4>Silahkan Masukkan Data Diri Bayi Yang<br>Anda Alami</h4>
+      </div>
+      <form action="">
+        <div class="row">
+          <div class="col-sm-6 form-group">
+            <label for="">Nama Bayi</label>
+            <input type="text" name="name" class="form-control" id="name"  required> <br>
+            <label for="">Usia Bayi</label>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Bulan" required> <br>
+            <label for="">Tinggi Badan Bayi</label>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Cm" required> <br>
+            <label for="">Berat Badan Bayi</label>
+            <input type="text" name="name" class="form-control" id="name" placeholder="Kg" required> <br>
+          </div>
+          <div class="col-sm-6 form-group">
+            <label for="">Nama Ibu</label> 
+            <input type="text" name="name" class="form-control" id="name" required> <br>
+            <label for="">Usia Ibu</label>
+            <input type="text" name="name" class="form-control" id="name" required> <br>
+          </div>
+        </div>
+        <br>
+        <div class="text-center"><button type="submit" class="btn btn-success">Send Message</button></div>
+      </form>
+
+</main><!-- End #main -->
+
+@stop
