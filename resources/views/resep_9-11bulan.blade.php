ba2a66a9d50d1b92c3824aa2bee374da29db8828
Nurakmal r <Nurakmal.ramadhan09@gmail.com>
2023-07-30 13:00:45 +0700

    add resep 9-11 bulan

---
+++ resources/views/resep_9-11bulan.blade.php
@@ -0,0 +1,137 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main id="main">
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/resep_9-11bulan/head.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Resep Untuk Usia 9-11 Bulan</h2>
+                <!-- <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
+                <div>
+                  <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="row no-gutters">
+        <h4>Nasi Tim Ikan Tuna Telur Puyuh</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_9-11bulan/content_1.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_9-11bulan/info_1.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>115 gr (12 sdm) nasi putih</li>
+            <li>30 gr (1 potong kecil) ikan tuna segar, haluskan</li>
+            <li>30 gr (3 butir) telur puyuh</li>
+            <li>15 gr (1 potong besar) wortel</li>
+            <li>10 gr (1 buah sedang tomat</li>
+            <li>7.5 gr (1.5 sdt) minyak kelapa</li>
+            <li>75 cc (1/3 gelas belimbing) kaldu ayam</li>
+            <li>50 gr (1/2 potong) pepaya, haluskan</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>180 gr (1 potong besar) semangka</li>
+          </ul>
+          <b>Cara Membuat Bubur:</b>
+          <ul>
+            <li>Masukkan nasi, ikan tuna, telur puyuh, minyak kelapa ke dalam mangkok tim</li>
+            <li>Tambahkan air kaldu</li>
+            <li>Masukkan wortel dan tomat, tim hingga matang</li>
+            <li>Angkat, sajikan dengan saos papaya.</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Nasi Tim Ayam Lele Cincang</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_9-11bulan/content_2.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_9-11bulan/info_2.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>100 gr (10 sdm) nasi putih</li>
+            <li>30 gr (3 sdm) daging ayam cincang</li>
+            <li>10 gr (1 sdm) daging ikan lele</li>
+            <li>10 gr (1 sdm) wortel</li>
+            <li>5 ml (1 sdt) minyak goreng</li>
+            <li>10 ml (1 sdm) santan kental</li>
+            <li>1.5 sdm bawang bombay (iris halus)</li>
+            <li>200 ml kaldu ayam</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>180 gr (1 potong besar) semangka</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Masukkan nasi, daging ayam cincang, ikan lele, bawang bombay, minyak dan santan ke dalam mangkok tim.</li>
+            <li>Tambahkan air kaldu</li>
+            <li>Masukkan wortel, masak hingga lunak dan matang</li>
+            <li>Angkat dan sajikan</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Mie Kukus Telur Puyuh</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_9-11bulan/content_3.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_9-11bulan/info_3.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>85 gr (3 bungkus) mie instan batita atau mie keriting</li>
+            <li>60 gr (6 butir) butir Telur puyuh</li>
+            <li>50 gr ( 5 sdm) wortel parut</li>
+            <li>50 gr (5 sdm) keju parut</li>
+            <li>10 gr (1 batang) bawang daun iris</li>
+            <li>Minyak untuk menggoreng</li>
+            <li>Bumbu mie instan batita secukupnya</li>
+            <li>300 ml Air</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>150 gr (6 potong kecil) Pepaya</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Didihkan air sebanyak 300 ml masukkan mie instan balita atau mie keriting sampai lunak dan matang.</li>
+            <li>Masak mie instan balita atau mie keriting hingga matang.</li>
+            <li>Tambahkan wortel parut, bawang daun, keju parut , dan bumbu mie instan batita secukupnya, kembali aduk sampai merata</li>
+            <li>Masukkan 1 sdm adonan mie ke dalam wadah tahan panas yg sudah diolesi minyak dan masukan telur puyuh yang sudah direbus. Lakukan sampai adonan habis</li>
+            <li>Kukus selama 15 menit Setelah matang boleh langsung dikonsumsi atau digoreng sampai kuning keemasan. Hidangkan.</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End About Section -->
+</main><!-- End #main -->
+
+@stop
