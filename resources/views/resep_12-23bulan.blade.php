27845ba4158eb305cd55ddbbde88a26e341356eb
Nurakmal r <Nurakmal.ramadhan09@gmail.com>
2023-07-30 13:01:05 +0700

    add resep 12-23 bulan

---
+++ resources/views/resep_12-23bulan.blade.php
@@ -0,0 +1,175 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main id="main">
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/resep_12-23bulan/head.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Resep Untuk Usia 12-23 Bulan</h2>
+                <!-- <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
+                <div>
+                  <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="row no-gutters">
+        <h4>Nasi Sup Telur Puyuh Bola Tahu Ayam</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_12-23bulan/content_1.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_12-23bulan/info_1.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>150 gr (15 sdm) nasi putih</li>
+            <li>45 gr (1 potong kecil ) tahu putih</li>
+            <li>60 gr (± 5 sdm) daging ayam cincang</li>
+            <li>60 gr (6 butir) telur puyuh rebus</li>
+            <li>30 gr (3 sdm) wortel</li>
+            <li>30 gr (3 sdm) jagung pipil kuning</li>
+            <li>30 gr (3 sdm) brokoli</li>
+            <li>Air secukupnya</li>
+          </ul>
+          <b>Bumbu:</b>
+          <ul>
+            <li>10 gr (1 batang) seledri, diiris</li>
+            <li>10 gr (1 batang) daun bawang, diiris</li>
+            <li>15 gr (± 1.5 sdm) tepung tapioka</li>
+            <li>Garam secukupnya</li>
+            <li>Gula Secukupnya</li>
+            <li>Merica secukupnya</li>
+            <li>10 gr (1 sdm) bawang goreng</li>
+          </ul>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>135 gr (3 potong) Melon</li>
+          </ul>
+          <b>Cara Membuat Bubur:</b>
+          <ul>
+            <li>Rebus air sampai mendidih. Sambil menunggu air campurkan tahu, daging ayam cincang, bumbu halus, garam, gula, dan merica, selanjutnya haluskan serta tambahkan tapioka kemudian uleni hingga tercampur rata dan bisa di bentuk</li>
+            <li>Ambil adonan sebanyak satu sendok teh kemudian bentuk bulat dan langsung di masukkan pada air mendidih.</li>
+            <li>Lakukan sampai adonan habis Setelah semua bola-bola tahu ayam mengapung masukan wortel dan jagung terlebih dulu tunggu sejenak kemudian brokoli dan telur puyuh rebus</li>
+            <li>Masukan garam, gula dan merica serta irisan bawang daun dan seledri. Setelah sayuran matang koreksi rasa. Sajikan dengan nasi dan taburan bawang goreng</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Nasi Soto Ayam Kuah Kuning</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_12-23bulan/content_2.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_12-23bulan/info_2.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>150 gr (15 sdm) nasi</li>
+            <li>100 gr (2 potong ayam dengan tulang)</li>
+            <li>10 gr (1 sdm) daging ikan lele</li>
+            <li>10 gr (1 sdm) soun</li>
+            <li>60 gr (6 butir) telur puyuh</li>
+            <li>30 gr (3 sdm) toge</li>
+            <li>10 gr (1 sdm) minyak goreng</li>
+          </ul>
+          <b>Bumbu:</b>
+          <ul>
+            <li>1 lembar daun salam</li>
+            <li>1 batang serai, digeprek</li>
+            <li>1 lembar daun jeruk</li>
+            <li>1/2 cm lengkuas, digeprek</li>
+            <li>1500 air untuk merebus</li>
+            <li>garam dan gula secukupnya</li>
+            <li>merica secukupnya</li>
+          </ul>
+          <b>Bumbu Halus:</b>
+          <ul>
+            <li>1 butir kemiri</li>
+            <li>1 siung bawang merah</li>
+            <li>1 siung bawang putih</li>
+            <li>1/2 cm jahe</li>
+            <li>1/2 cm kunyit</li>
+            <li>1.4 sdt jinten</li>
+            <li>1/2 sdt ketumbar</li>
+          </ul>
+          <b>Pelengkap:</b>
+          <ul>
+            <li>1/2 sdt irisan daun bawang</li>
+            <li>1/2 sdt irisan seledri</li>
+            <li>1/3 sdt irisan bawang goreng</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Rebus ayam hingga setengah matang, tambahkan serai geprek, daun salam, daun jeruk, dan lengkuas yang digeprek</li>
+            <li>Tumis bumbu yang dihaluskan hingga harum dan matang, lalu masukkan ke dalam rebusan ayam.</li>
+            <li>Tambahkan garam, gula dan merica. Masak hingga ayam matang dan empuk, (jangan lupa koreksi rasa)</li>
+            <li>Daging ayam bisa diangkat dan di suir-suir</li>
+            <li>Siapkan mangkuk berikan soun yang sudah direndam, toge, suiran daging ayam, serta telur puyuh rebus</li>
+            <li>Tambahkan kuah soto ayam, serta taburkan daun bawang, seledri, dan bawang goreng</li>
+            <li>Sajikan dengan nasi hangat dan lengkapi dengan buah.</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Sup Telur Puyuh Ikan Air Tawar Labu Kuning</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_12-23bulan/content_3.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_12-23bulan/info_3.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>150 gr (15 sdm) nasi putih</li>
+            <li>70 gr (7 sdm) kentang, potong dadu</li>
+            <li>50 gr (5 sdm) labu kuning, potong dadu</li>
+            <li>120 gr (12 sdm) ikan nila kukus, disuwir</li>
+            <li>60 gr (6 butir) telur puyuh, rebus</li>
+            <li>15 gr (3 sdt) kacang merah</li>
+            <li>45 gr (4.5 sdm) tomat, potong dadu</li>
+            <li>10 gr (1 batang) bawang daun</li>
+            <li>5 gr (1 sdt) bawang merah goreng</li>
+            <li>5 gr (1 sdt) bawang putih goreng</li>
+            <li>150 ml kaldu ayam</li>
+            <li>Garam, gula, merica secukupnya</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>150 gr (6 potong) pepaya</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Rebus kaldu ayam hingga mendidih, masukkan kentang, kacang merah, dan labu kuning hingga setengah matang.</li>
+            <li>Masukkan suwiran ikan nila kukus, bawang merah goreng dan bawang putih goreng masak hingga matang.</li>
+            <li>Tambahkan tomat, bawang daun, bawang goreng, dan telur puyuh, aduk perlahan.</li>
+            <li>Tambahkan garam, gula dan merica secukupnya dan aduk rata. Angkat dan sajikan dengan buah.</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End About Section -->
+</main><!-- End #main -->
+
+@stop
