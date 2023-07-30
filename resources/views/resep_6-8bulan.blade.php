96c99bb8d0e7909dc7e63c75833370285be7d227
Nurakmal r <Nurakmal.ramadhan09@gmail.com>
2023-07-30 12:59:53 +0700

    add resep 6-8 bulan

---
+++ resources/views/resep_6-8bulan.blade.php
@@ -0,0 +1,164 @@
+@extends('layouts.master')
+
+@section('content')
+
+<main id="main">
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero">
+    <div class="hero-container">
+      <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+        <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <!-- Slide 1 -->
+          <div class="carousel-item active" style="background-image: url(assets/img/resep_6-8bulan/head.jpg);">
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2 class="animate__animated animate__fadeInDown">Resep Untuk Usia 6-8 Bulan</h2>
+                <!-- <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p> -->
+                <div>
+                  <!-- <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section><!-- End Hero -->
+
+  <section id="about" class="about">
+    <div class="container">
+
+      <div class="row no-gutters">
+        <h4>Bubur Singkong isi ikan dan ayam dengan saus jeruk</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_6-8bulan/content_1.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_6-8bulan/info_1.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>75 gr singkong putih, rebus dan haluskan</li>
+            <li>15 gr (2 sdm datar) daging ikan kembung cincang halus</li>
+            <li>15 gr daging ayam cincang rebus</li>
+            <li>250 ml air kaldu ayam</li>
+            <li>5 gr (1 sdt) minyak kelapa</li>
+            <li>20 gr (2 sdm) bayam segar, potong halus</li>
+          </ul>
+          <b>Bumbu:</b>
+          <ul>
+            <li>1 lembar daun salam</li>
+            <li>1 batang sereh</li>
+          </ul>
+          <b>Bumbu Halus:</b>
+          <ul>
+            <li>1 siung bawang merah</li>
+            <li>1 siung bawang putih</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>100 gr (3 buah kecil) jeruk manis diambil sarinya</li>
+          </ul>
+          <b>Cara Membuat Bubur:</b>
+          <ul>
+            <li>Tumis bumbu halus, lalu masukkan daun salam dan sereh.</li>
+            <li>Tambahkan air kaldu, masukkan singkong putih, daging ikan, daging ayam cincang rebus, aduk-aduk hingga setengah matang.</li>
+            <li>Masukkan daun bayam, aduk hingga matang. Jika airnya mengental dapat ditambahkan air matang.</li>
+            <li>Angkat, lalu saring halus atau diblender. Sebelum disajikan tambahkan saus jeruk.</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Bubur Soto Ayam Santan</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_6-8bulan/content_2.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_6-8bulan/info_2.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>60 gr (6 sdm) Nasi putih</li>
+            <li>45 gr (4.5 sdm) Daging</li>
+            <li>ayam cincang</li>
+            <li>30 gr (1 buah kecil) Tahu</li>
+            <li>30 gr (3 sdm) Labu siam</li>
+            <li>15 gr (1.5 sdm) wortel</li>
+            <li>1 lembar Salam</li>
+            <li>1 batang Sereh</li>
+            <li>1 lembar Daun jeruk</li>
+            <li>5 gr (1 sdm) Minyak goreng</li>
+            <li>30 ml (3 sdm) Santan</li>
+            <li>300 ml Air kaldu ayam</li>
+          </ul>
+          <b>Bumbu Halus:</b>
+          <ul>
+            <li>1 siung bawang merah</li>
+            <li>1 siung bawang putih</li>
+            <li>1 cm Kunyit</li>
+            <li>1 cm Jahe</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>100 gr (3 buah kecil) Jeruk (diambil sarinya)</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Tumis bumbu halus sampai harum, masukan ayam cincang sampai berubah warna.</li>
+            <li>Masukan air kaldu ayam, santan, salam, sereh dan daun jeruk masak sampai mendidih.</li>
+            <li>Masukan nasi, tahu dan labu siam dan wortel yang sudah diiris kecil-kecil masak sampai semua bahan matang dan empuk.</li>
+            <li>Haluskan sampai tekstur yang diinginkan. Sajikan selagi hangat</li>
+          </ul>
+        </div>
+      </div><br>
+
+      <div class="row no-gutters">
+        <h4>Bubur Sup Daging Kacang Merah</h4>
+        <div class="col col-6">
+          <img src="assets/img/resep_6-8bulan/content_3.jpg" alt="" style="width:100%;max-width:600px;height:auto;">
+          <img src="assets/img/resep_6-8bulan/info_3.jpg" alt="" style="width:100%;max-width:400px;height:auto;">
+        </div>
+        <div class="col col-6">
+          <b>Bahan:</b>
+          <ul>
+            <li>50 gr (6 sdm) nasi</li>
+            <li>30 gr (3 sdm) daging ayam cincang</li>
+            <li>25 gr (1/2 butir) telur ayam</li>
+            <li>10 gr (1 sdm) buncis</li>
+            <li>10 gr (1 sdm) wortel</li>
+            <li>10 gr (1 sdm) kacang merah</li>
+            <li>10 gr (1 batang) bawang daun</li>
+            <li>1 batang seledri</li>
+            <li>300 ml kaldu ayam</li>
+            <li>2.5 gr (1/2 sdt) minyak untuk menumis.</li>
+          </ul>
+          <b>Bumbu Halus:</b>
+          <ul>
+            <li>2 siung bawang merah</li>
+            <li>2 siung bawang putih</li>
+          </ul>
+          <b>Buah:</b>
+          <ul>
+            <li>100 g (2 buah) jeruk diambil sarinya</li>
+          </ul>
+          <b>Cara Membuat:</b>
+          <ul>
+            <li>Didihkan air kaldu ayam, masukkan kacang merah dan masak sampai empuk</li>
+            <li>Tumis bumbu halus sampai harum, masukkan daging ayam cincang, masak sampai berubah warna</li>
+            <li>Masukan tumisan daging ayam kedalam air kaldu masak sampai daging empuk</li>
+            <li>Masukkan nasi, buncis, dan wortel.</li>
+            <li>Tambahkan kocokan telur, aduk merata dan masak sampai matang</li>
+            <li>Haluskan bubur sampai tekstur yang diinginkan, lalu sajikan</li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  </section><!-- End About Section -->
+</main><!-- End #main -->
+
+@stop
